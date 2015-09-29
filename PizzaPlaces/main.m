43c0f82189f3bd29e9d934ed49c9c723fe305797
Oleg Lavrentyev <oleg@olearis.com>
2015-09-29 14:11:12 +0300

    Initial Commit

---
+++ PizzaPlaces/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PizzaPlaces
+//
+//  Created by Oleg Lavrentyev on 9/29/15.
+//  Copyright © 2015 Olearis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
